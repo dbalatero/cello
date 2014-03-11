4152c0915541e1774a10374593bb249b65e3cfbd
joshuarule <joshua@joshuarule.com>
2014-03-11 16:59:59 -0400

    added js to scroll the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,26 @@
+function scrollToElement(selector, time, verticalOffset) {
+    time = typeof(time) != 'undefined' ? time : 500;
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $(selector);
+    offset = element.offset();
+    offsetTop = offset.top + verticalOffset;
+    $('html, body').animate({
+        scrollTop: offsetTop
+    }, time);
+}
 
+$('nav .link-2').click(function () {
+    event.preventDefault();
+    scrollToElement('#about');
+});
+
+$('nav .link-3').click(function () {
+    event.preventDefault();
+    scrollToElement('#value');
+});
+
+$('nav .link-4').click(function () {
+    event.preventDefault();
+    scrollToElement('#location');
+});
 
